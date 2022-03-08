e5f15e23fd002f1c91272b8bccb0b28ef964b6b7
Gengsu07 <82944512+Gengsu07@users.noreply.github.com>
2022-03-08 11:01:20 +0700

    Update dashtim.py

--- dashtim.py
+++ dashtim.py
@@ -3,14 +3,12 @@ from pandas.core import groupby
 from sqlalchemy import create_engine
 import plotly.graph_objs  as go
 
-conn =create_engine('postgresql://postgres:sgwi2341@10.4.19.215/penerimaan')
+# conn =create_engine('postgresql://postgres:sgwi2341@10.4.19.215/penerimaan')
 
-query_mpnspm = '''select "KDMAP","DATEBAYAR",sum("JUMLAH") as Nominal 
-from appportal.mpnspm_2022
-group by "KDMAP","DATEBAYAR"
-'''
+# query_mpnspm = '''select "KDMAP","DATEBAYAR",sum("JUMLAH") as Nominal 
+# from appportal.mpnspm_2022
+# group by "KDMAP","DATEBAYAR"
+# '''
+superstore = pd.read_excel('superstore.xls')
 
-mpnspm = pd.read_sql(query_mpnspm,con=conn)
-mpnspm.head()
-
-
+scat = [go.Scatter(x = mpnspm[''])]
